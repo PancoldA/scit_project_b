9c3358e9eebac9aa25ade24e9b7f48b5a29db177
PancoldA <didxogus789@naver.com>
2020-03-24 19:58:59 +0900

    bootstrap style test 적용

--- src/main/java/global/sesoc/wareware/HomeController.java
+++ src/main/java/global/sesoc/wareware/HomeController.java
@@ -1,39 +1,25 @@
 package global.sesoc.wareware;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	@GetMapping(value = "/")
+	public String index01() {
+		logger.info("HomeController's index Method");
+		return "index";
+	}
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+	@GetMapping(value = "/index")
+	public String index02() {
+		logger.info("HomeController's index Method");
+		return "index";
 	}
 	
 }
